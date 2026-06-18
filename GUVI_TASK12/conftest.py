84887fde83dd5c3a614dd54216af39e32139be6d
Patta <lokesh.patta@OURLOTTO.COM>
2026-06-18 11:24:43 +0530

    Guvi_Task12

---
+++ GUVI_TASK12/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+# @pytest.fixture
+# def driver():
+#     options = Options()
+#     options.add_argument("--headless=new")
+#     options.add_argument("--window-size=1920,1080")
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+#     yield driver
+#     driver.quit()
