124087fd4686847d034d63dc754803b6ccc7cab8
Patta <lokesh.patta@OURLOTTO.COM>
2026-06-29 13:08:13 +0530

    GUVI_Task13

--- GUVI_Task13/Guvi_Task13.py
+++ GUVI_Task13/Guvi_Task13.py
@@ -3,16 +3,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
-import time
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://jqueryui.com/droppable/")
 driver.maximize_window()
-time.sleep(3)
 iframe = driver.find_element(By.XPATH,"//iframe[@class='demo-frame']")
 driver.switch_to.frame(iframe)
 source = driver.find_element(By.XPATH,"//div[@id='draggable']")
 target = driver.find_element(By.XPATH,"//div[@id='droppable']")
 actions = ActionChains(driver)
 actions.drag_and_drop(source,target).perform()
-time.sleep(5)
 driver.switch_to.default_content()
